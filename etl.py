c2e3b9f04786abe8517edd0af225c52929df6c20
David Ellis <david@guestfolio.com>
2019-08-18 16:08:18 -0700

    add table setup procedure to etl

--- etl.py
+++ etl.py
@@ -1,6 +1,6 @@
 import configparser
 import psycopg2
-from sql_queries import copy_table_queries, insert_table_queries
+from sql_queries import copy_table_queries, insert_table_queries, drop_table_queries, create_table_queries
 
 
 def load_staging_tables(cur, conn):
@@ -21,9 +21,25 @@ def insert_tables(cur, conn):
         cur.execute(query)
         conn.commit()
 
+def drop_tables(cur, conn):
+    """Drop all tables if they exist"""
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def create_tables(cur, conn):
+    """Create all tables"""
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
 
 def main():
-    """Entrypoint for the full ETL process"""
+    """Entrypoint for the full ETL process
+
+    Warning: will drop existing tables.
+    """
 
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
@@ -31,6 +47,8 @@ def main():
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
